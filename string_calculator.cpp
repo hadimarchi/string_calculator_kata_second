b0c83915217b0b7c7643f28e8a34370d46983f67
Chris Bailey <chris@chrisbailey.io>
2018-02-14 15:54:35 -0900

    Pass first test

--- string_calculator.cpp
+++ string_calculator.cpp
@@ -4,28 +4,24 @@
 using std::vector;
 #include <string>
 using std::string;
+#include <sstream>
+using std::stringstream;
 
-string delimiter = ",";
 int add(const string &addable){
     vector<int> nums{};
     if(addable.size()==0){
         return 0;
     }
-
-    size_t numnums = std::count(addable.begin(), addable.end(), ',');
-
-    for(unsigned int i = 0; i < numnums * 2; i+=2){
-      nums.push_back(stoi(addable.substr(i, addable.find(delimiter))));
-    }
-    auto num1 = stoi(addable.substr(0, addable.find(delimiter)));
-    int num2 = 0;
-    try{
-      auto num2 = stoi(addable.substr(2, addable.find(delimiter)));
-      return num1 + num2;
-    }
-    catch(...){
-      return num1;
-    }
+		unsigned int total = 0;
+    stringstream ss(addable);
+		unsigned int i = 0;
+		while (ss >> i) {
+			total += i;
+			if (ss.peek() == ',' || ss.peek() == '\n') {
+				ss.ignore();
+			}
+		}
+		return total;
 }
 
 TEST_CASE( "add null str [add]" ) {
